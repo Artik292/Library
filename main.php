62f5d7fd0cc431e8fc861d7188621ba31e44e043
Artur Konevnikov <artik292@gmail.com>
2017-07-09 11:32:00 +0300

    updating file

--- main.php
+++ main.php
@@ -14,6 +14,6 @@ if (isset($_SESSION['user_name'])) {
   $app->layout->add(['Message','List of books']);
 }
 //$layout = $app->layout;
-$grid = $layout->add('GRID');
+$grid = $layout->add('grid');
 $grid->setModel(new book($db));
 $grid->addQuickSearch(['book_title','author']);
