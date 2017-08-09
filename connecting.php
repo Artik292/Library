f23ecd8629c52e3557d9adb4c3869c619c81a245
Romans Malinovskis <romans@agiletoolkit.org>
2017-08-09 12:19:20 +0100

    Cleaned up file 'connecting'

--- connecting.php
+++ connecting.php
@@ -1,109 +1,93 @@
 <?php
+
 require 'vendor/autoload.php';
 
+
+// Set up things which we will need on all of our pages, such as database connection,
+// session and the application.
+$app = new \atk4\ui\App('Librray');
 session_start();
-
 if (isset($_ENV['CLEARDB_DATABASE_URL'])) {
-    preg_match('|([a-z]+)://([^:]*)(:(.*))?@([A-Za-z0-9\.-]*)(/([0-9a-zA-Z_/\.]*))|',
-        $_ENV['CLEARDB_DATABASE_URL'],$matches);
-    $dsn=array(
-        $matches[1].':host='.$matches[5].';dbname='.$matches[7],
-        $matches[2],
-        $matches[4]
-    );
-    $db = new \atk4\data\Persistence_SQL($dsn[0].';charset=utf8', $dsn[1], $dsn[2]);
+    $db = new \atk4\data\Persistence_SQL($_ENV['CLEARDB_DATABASE_URL']);
 } else {
-    //$db = \atk4\db\Persistence::connect('mysql://user:pass@localhost/main');
-    //$db = \atk4\db\Persistence::connect('mysql://user:pass@localhost/mydb');
-    $db = new \atk4\data\Persistence_SQL('mysql:host=127.0.0.1;dbname=mydb;charset=utf8', 'root', '');
-}
-
-class student extends \atk4\data\Model {
-	public $table = 'student';
-
-  public $login_field = 'nickname';
-  public $password_field = 'password';
-  public $title_field = 'name';
-function init() {
-	parent::init();
-	$this->addField('name',['required'=>'true']);
-	$this->addField('surname',['required'=>'true']);
-	$this->addField('grade',['required'=>'true']);
-  $this->addField('nickname',['required'=>'true']);
-  $this->addField('password',['type'=>'password','required'=>'true']);
-}
-}
-
-class book extends \atk4\data\Model {
-	public $table = 'book';
-  public $title_field = 'book_title';
-  public $tible_name = 'book_title';
-function init() {
-	parent::init();
-	$this->addField('book_title');
-	$this->addField('author');
-	$this->addField('year_published',['type'=>'date']);
-  $this->addField('total_quantity');
-}
-}
-
-  class borrow extends \atk4\data\Model {
-  	public $table = 'borrow';
-  function init() {
-  	parent::init();
-  	$this->addField('date_loan',['type'=>'date']);
-  	$this->addField('date_return',['type'=>'date']);
-    $this->addField('returned', ['type'=>'boolean']);
-    $this->addField('quantity');
-    $this->hasOne('book_id', new book())->addTitle();
-    $this->hasOne('student_id', new student())->addTitle();
-  }
+    $db = new \atk4\data\Persistence_SQL('mysql:host=127.0.0.1;dbname=library;charset=utf8', 'root', 'root');
 }
 
 
-/*class student extends \atk4\data\Model {
-	public $table = 'student';
+// Declare classes for our models, which we will use in our application
 
-  public $login_field = 'name';
-  public $password_field = 'password';
-function init() {
-	parent::init();
-	$this->addField('name',['required'=>'true']);
-	$this->addField('surname',['required'=>'true']);
-	$this->addField('grade',['required'=>'true']);
-  $this->addField('password',['type'=>'password','required'=>'true']);
-}
-}
-class book extends \atk4\data\Model {
-	public $table = 'book';
-function init() {
-	parent::init();
-	$this->addField('name',['caption'=>'Book title','required'=>'true']);
-	$this->addField('author');
-	$this->addField('year_published',['type'=>'date']);
-  $this->addField('total_quantity',['required'=>'true']);
-}
-}
-class librarian extends \atk4\data\Model {
-	public $table = 'librarian';
-function init() {
-	parent::init();
-	$this->addField('name',['required'=>'true']);
-	$this->addField('surname',['required'=>'true']);
-  $this->addField('password',['type'=>'password','required'=>'true']);
-}
-}
-  class borrow extends \atk4\data\Model {
-  	public $table = 'borrow';
-  function init() {
-  	parent::init();
-  	$this->addField('date_loan',['type'=>'date','required'=>'true']);
-  	$this->addField('date_return',['type'=>'date','required'=>'true']);
-    If ($_SESSION['status'] != 'student') {
-        $this->addField('returned', ['type'=>'boolean']);
+/**
+ * Describes school student who will be allowed to check out books from the library.
+ */
+class Student extends \atk4\data\Model {
+    public $table = 'student';
+
+    function init() {
+        parent::init();
+
+        // Declaration of basic fields
+        $this->addField('name', ['required'=>true]);
+        $this->addField('grade', ['required'=>true]);
+        $this->addField('password', ['type'=>'password', 'required'=>true]);
+
+        // Referencing all the check outs students have made
+        $this->hasMany('CheckOut', new CheckOut());
     }
-    $this->addField('quantity',['required'=>'true']);
-    $this->hasOne('book_id', new book())->addTitle();
-    $this->hasOne('student_id', new student())->addTitle();
-  }
-} */
+}
+
+class Book extends \atk4\data\Model {
+    public $table = 'book';
+
+    function init() {
+        parent::init();
+        $this->addField('name', ['caption'=>'Book title', 'required'=>true]);
+        $this->addField('author');
+        $this->addField('year_published', ['type'=>'date']);
+
+        // How many book copies this Library owns?
+        $this->addField('total_quantity', ['required'=>true]);
+
+        // Referencing all the check outs of this book
+        $this->hasMany('CheckOut', new CheckOut());
+    }
+}
+
+class Librarian extends \atk4\data\Model {
+    public $table = 'librarian';
+
+    function init() {
+        parent::init();
+        $this->addField('name', ['required'=>true]);
+        $this->addField('surname', ['required'=>true]);
+        $this->addField('password', ['type'=>'password','required'=>true]);
+
+        // Referencing all the check outs authorized by this librarian
+        $this->hasMany('CheckOut', new CheckOut());
+    }
+}
+
+class CheckOut extends \atk4\data\Model {
+    public $table = 'checkout';
+
+    function init() {
+        parent::init();
+
+        // Date when the book was checked out from the library
+        $this->addField('date_checked_out', ['type'=>'date','required'=>true]);
+
+        // Date when book is due to be returned
+        $this->addField('date_return',['type'=>'date','required'=>'true']);
+
+        // Will be set to true, once the book is returned
+        $this->addField('returned', ['type'=>'boolean']);
+
+        $this->hasOne('book_id', new Book())
+            ->addTitle();
+
+        $this->hasOne('student_id', new Student())
+            ->addTitle();
+
+        $this->hasOne('librarian_id', new Librarian())
+            ->addTitle();
+    }
+}
